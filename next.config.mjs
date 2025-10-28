1bb99f0cebd0c51c417f29b7f38a914216814bed
axelCaballeroTR <Axel.Caballero@thomsonreuters.com>
2025-10-28 16:37:47 -0600

    fixed routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/OS3-Synergy-default-dashboard",
+  assetPrefix: "/OS3-Synergy-default-dashboard",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
